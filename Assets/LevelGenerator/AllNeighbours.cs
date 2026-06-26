e7c3cbe0d69b9b3de1c1a958f0dd49e228cbb892
Hoàng Ngọc Đức <days2408@gmail.com>
2026-06-26 18:16:27 +0700

    all neighbour

--- Assets/LevelGenerator/AllNeighbours.cs
+++ Assets/LevelGenerator/AllNeighbours.cs
@@ -1,14 +1,35 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class AllNeighbours : MonoBehaviour
+public class AllNeighbours : MonoBehaviour,IGenerateMethod
 {
-     void Start()
-       {
-       		 
-        }
+    [SerializeField] private TextMeshProUGUI _timeText, _gridCountText;
+    private List<GridData> _checkingGrid;
+    private LevelGenerator Instance;
+    private bool isCreating;
 
-     void Update()
-       {
-       		 
-        }
+    [SerializeField] private float speedMultipler;
+    [SerializeField] private float speed;
+
+    private void Start()
+    {
+        _checkingGrid = new List<GridData>();
+        Instance = GetComponent<LevelGenerator>();
+    }
+
+    public void Generate()
+    {
+        
+    }
+}
+
+public class GridData
+{
+    public GridData()
+    {
+
+    }
+
 }
